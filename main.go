66bda140514436fff2ba36dc6586aad4eca15f19
piotr-szyk <student-01-7a5cac163934@qwiklabs.net>
2026-03-27 17:15:14 +0000

    Truly added Red handler to main.go

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
+
 import (
 	"fmt"
 	"net/http"
 )
+
 func main() {
 	http.HandleFunc("/blue", blueHandler)
 	http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
+
 func blueHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body style='background-color:blue;'></body></html>")
 }
+
 func redHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body style='background-color:red;'></body></html>")
 }
